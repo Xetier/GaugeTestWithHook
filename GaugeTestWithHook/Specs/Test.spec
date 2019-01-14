9e1d77998855b6fa9f55d05298a26adc84584d44
Pieter Gleixner <pieter.gleixner@cignium.com>
2019-01-14 14:18:14 -0500

    Initial commit

---
+++ GaugeTestWithHook/Specs/Test.spec
@@ -0,0 +1,12 @@
+﻿Test
+==========
+tags: testspec
+
+Test1
+-----------------
+tags: testescenario1
+* Go to "https://duckduckgo.com/"
+* Click to search Field
+* Click to button search
+* Click first link
+* Check title text "C# Guide" exist
---
+++ GaugeTestWithHook/Steps/DuckSteps.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GaugeTestWithHook
+{
+    public class DuckSteps
+    {
+        IWebDriver _driver = Hooks.WebDriver;
+
+
+    }
+}
